d09bc2f1c0af1a502330601d15bd5c0f7a9bd7cf
Marcos <marcosfelipetur@gmail.com>
2025-12-02 21:18:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6811,11 +6811,11 @@ function updateReportFiltersUI() {
         document.getElementById('filters-integracoes').style.display = 'grid'; 
     }
     else if (type !== '') { 
-        document.getElementById('filters-generic').style.display = 'grid'; }
+        document.getElementById('filters-generic').style.display = 'grid';
     }
     else if (type === 'colaboradores') { 
-        document.getElementById('filters-colaboradores').style.display = 'grid'; }
-    
+        document.getElementById('filters-colaboradores').style.display = 'grid';
+    }
     else if (type !== '') { 
         document.getElementById('filters-generic').style.display = 'grid'; }
 }
