99177a91fa6dd1d446e6947f62eb911b92deb028
Marcos <marcosfelipetur@gmail.com>
2025-12-11 15:17:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6935,8 +6935,8 @@ function atualizarTabelaIntegracoesPaginada() {
             <thead>
                 <th>Município</th>
                 <th>APIs Em Funcionamento no Município</th>
-                <th>Responsável Por<br>Atualizar Certificado</th>
-                <th>Contato do<br>Responsável</th> 
+                <th>Responsável Certificado</th>
+                <th>Contato</th> 
                 <th>Data de<br>Vencimento</th>
                 <th>Status Vencimento</th>
                 <th>Observações</th>
